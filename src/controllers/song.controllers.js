d0bbf020afbc9f01b3b731b2fcb503ab7a651b8a
Edumanning <edugonzalez001@gmail.com>
2023-07-30 23:14:41 -0500

    actualizacion getOne, remove, update

--- src/controllers/song.controllers.js
+++ src/controllers/song.controllers.js
@@ -11,7 +11,34 @@ const create = catchError(async (req, res) => {
     return res.status(201).json(song)
 });
 
+const getOne = catchError(async (req,res) => {
+    const { id } = req.params
+    const song = await Song.findByPk(id)
+    if(!song) return res.status(404).json({message: "Song not found"})
+    return res.json(Song)
+})
+
+const remove = catchError(async (req,res) => {
+    const { id } = req.params
+    const removeSong = await Song.destroy({ where: { id }})
+    if(!removeSong) return res.status(404).json({message: "Song not found"})
+
+    return res.sendStatus(204)
+})
+
+const update = catchError(async(req,res) =>{
+    const { id } = req.params
+    const song = req.body
+
+    const songUpDate = await Song.update(song, {where: {id}, returning: true})
+    if(songUpDate[0] === 0 ) return res.status(404).json({message: "Song not found"})
+    return res.json(songUpDate[1][0]) 
+})
+
 module.exports = {
     getAll,
-    create
+    create,
+    getOne,
+    remove,
+    update
 }
--- src/routes/song.router.js
+++ src/routes/song.router.js
@@ -1,4 +1,4 @@
-const { getAll, create } = require('../controllers/song.controllers');
+const { getAll, create, getOne, remove, update } = require('../controllers/song.controllers');
 const express = require('express');
 
 const songRouter = express.Router();
@@ -6,5 +6,10 @@ const songRouter = express.Router();
 songRouter.route("/")
     .get(getAll)
     .post(create)
+
+songRouter.route("/:id")
+    .get(getOne)
+    .delete(remove)
+    .put(update)
 
 module.exports = songRouter;
